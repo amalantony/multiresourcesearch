59463df62ecd3faf44a1f47c4206dd875706441b
Amal Antony <amalantony06@gmail.com>
2017-01-28 05:46:29 +0530

    Handle the lack of url query params gracefully

--- main.go
+++ main.go
@@ -14,13 +14,21 @@ import (
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
+	w.Header().Set("Content-Type", "application/json")
+	if len(queryParams) < 1 {
+		errStructure := struct {
+			Message string
+		}{Message: "No Query Specified"}
+		errJSON, _ := json.Marshal(errStructure)
+		w.Write(errJSON)
+		return
+	}
 	query := queryParams["q"][0]
 	aggregatedResults := search.Search(query)
 	resultsJSON, err := json.Marshal(aggregatedResults)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("%s", err.Error()))
 	}
-	w.Header().Set("Content-Type", "application/json")
 	w.Write(resultsJSON)
 }
 
@@ -31,5 +39,6 @@ func main() {
 	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	fmt.Println("Listening on port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
